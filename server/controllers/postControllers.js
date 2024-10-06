8b39e9e20ea74a8b4be43e25dcc3b6404bfe7119
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-06 19:48:58 +0300

    added logic for getting and creating posts

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -1,6 +1,12 @@
-const getPosts = async () => {
-  const posts = await Post.find();
-  return posts;
+import PostMessage from "../models/postMessage.js";
+
+const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
 
 const getPost = async (id) => {
@@ -8,9 +14,15 @@ const getPost = async (id) => {
   return post;
 };
 
-const createPost = async (post) => {
-  const newPost = await Post.create(post);
-  return newPost;
+const createPost = async (req, res) => {
+  try {
+    const post = req.body;
+    const newPost = await PostMessage.create(post);
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(201).json({ message: error.message });
+  }
 };
 
 const updatePost = async (id, post) => {
