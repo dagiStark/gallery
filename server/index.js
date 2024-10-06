aeb1c775cfe0008829d6f5e36810e670e35bf707
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-06 16:57:45 +0300

    server and database connection estabilished

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const connectionString = process.env.MONGO_URL;
+const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(connectionString)
+  .then(() =>
+    app.listen(port, () => console.log(`Server is running on port ${port}`))
+  )
+  .catch((err) => console.log(err));
+
