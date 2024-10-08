7caedc428ee2c8e87e36d68ffcf2f1862e24a9d7
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-08 10:06:19 +0300

    payload and cors error prevented

--- server/index.js
+++ server/index.js
@@ -9,9 +9,9 @@ import postRoutes from "./routes/postRoutes.js";
 dotenv.config();
 const app = express();
 
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors({ origin: "http://localhost:5173" }));
 
 const connectionString = process.env.MONGO_URL;
 const port = process.env.PORT || 5000;
