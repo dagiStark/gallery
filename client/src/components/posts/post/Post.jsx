69e95a8b268f90053845437ec70307a58913d653
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-08 10:46:32 +0300

    single post component

--- client/src/components/posts/post/Post.jsx
+++ client/src/components/posts/post/Post.jsx
@@ -1,13 +1,59 @@
-import useStyles from './styles'
+import useStyles from "./styles";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  Box,
+} from "@mui/material";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import moment from "moment";
+import PropTypes from "prop-types";
 
-
-const Post = ({post}) => {
-  const classes = useStyles()
+const Post = ({ post }) => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>post</h1>
-    </div>
+    <Card>
+      <CardMedia />
+      <Box>
+        <Typography>{post.creator}</Typography>
+        <Typography>{moment(post.createdAt).fromNow()}</Typography>
+      </Box>
+      <Box>
+        <Button>
+          <MoreHorizIcon />
+        </Button>
+      </Box>
+      <Box>
+        <Typography>{post.tags.map((tag) => `#${tag} `)}</Typography>
+      </Box>
+      <CardContent>
+        <Typography>{post.message}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button>
+          <ThumbUpIcon /> Like {post.likeCount}
+        </Button>
+        <Button>
+          <DeleteForeverIcon /> Delete
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
 export default Post;
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    creator: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+    message: PropTypes.string.isRequired,
+    likeCount: PropTypes.number.isRequired,
+  }).isRequired,
+};
